367547b70b8940e0b807776a7767cef04b28a624
TristanLi67 <348813510@gapps.yrdsb.ca>
2025-11-10 11:41:17 -0800

    Hero move

--- DodgeitTeam-copy-copy/Hero.java
+++ DodgeitTeam-copy-copy/Hero.java
@@ -12,8 +12,16 @@ public class Hero extends Actor
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    boolean atTop = true;
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.mouseClicked(null){
+            atTop = !atTop;
+        }// Add your action code here.
+        if(atTop){
+            setLocation(100, 100);
+        } else {
+            setLocation(100, 300);
+        }
     }
 }
