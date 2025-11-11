90120f4bd8dbb6d1be0400be9fe597aa3a7b8bfd
AzlanAftabSadiq <349389189@gapps.yrdsb.ca>
2025-11-11 08:31:49 -0500

    Bomb movement cycle

--- dodge-it-team-tristan-azlan2/Bomb.java
+++ dodge-it-team-tristan-azlan2/Bomb.java
@@ -10,5 +10,19 @@ public class Bomb extends Actor
     public void act()
     {
         move(-10);
+        
+        if(getX() <= 0) {
+            resetBomb();
+        }
+    }
+    
+    public void resetBomb()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        } else {
+            setLocation(600, 300);
+        }
     }
 }
