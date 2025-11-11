33e830db1bacffce00ac533f342aaf9e8993f6a3
AzlanAftabSadiq <349389189@gapps.yrdsb.ca>
2025-11-11 08:30:01 -0500

    Bomb movement

--- dodge-it-team-tristan-azlan2/Bomb.java
+++ dodge-it-team-tristan-azlan2/Bomb.java
@@ -1,11 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Bomb here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Bomb extends Actor
 {
     /**
@@ -14,6 +9,6 @@ public class Bomb extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        move(-10);
     }
 }
